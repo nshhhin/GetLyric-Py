51e5029f9aa3b2637567b7c655159083a9882e62
nshhhin <shinn070110@gmail.com>
2020-03-21 14:02:55 +0900

    :sparkles: 歌詞ファイルを保存するようにした

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
-print('J-LyricのURLを入力してください')
-
-url = input()
+# utanetのurlを入れる
+url = 'https://www.uta-net.com/song/167259'
+fileName = url.split("/")[4]
 
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content, "html.parser")
 
-elms = soup.select("#Lyric")
+elms = soup.select("#flash_area")
+
+all_string = ""
 
 for elm in elms:
-    print(elm.text)
+   all_string += "\n" + elm.text
+with open("data/" + fileName + '.txt', 'w') as f:
+    f.write(all_string)
